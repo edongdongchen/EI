32cfeff2e1fefc30afccf8449812321b77fdb4f5
Dongdong Chen <43349309+edongdongchen@users.noreply.github.com>
2021-08-09 23:26:49 +0100

    Update train_paper_bash.sh

--- train_paper_bash.sh
+++ train_paper_bash.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'ei'
+python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'ei' --epochs 2000 --lr 1e-3 --schedule 500 1000 1500
 
-python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'sup'
+python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'sup' --epochs 2000 --lr 1e-3 --schedule 500 1000 1500
 
-python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'mc'
+python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'mc' --epochs 2000 --lr 1e-3 --schedule 500 1000 1500
 
-python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'sup_ei'
+python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'sup_ei' --epochs 2000 --lr 1e-3 --schedule 500 1000 1500
 
-python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'ei_adv'
+python3 demo_train.py --gpu 0 --task 'inpainting' --mode 'ei_adv' --epochs 2000 --lr 1e-3 --schedule 500 1000 1500
 
 python3 demo_train.py --gpu 0 --task 'ct' --mode 'ei' --epochs 5000 --lr 5e-4 --batch-size 2 --ei-trans 5 --ei-alpha 100 --schedule 2000 3000 4000
 
-python3 demo_train.py --gpu 0 --task 'ct' --mode 'sup' --epochs 5000 --lr 5e-4 --batch-size 2 --ei-trans 5 --ei-alpha 100 --schedule 2000 3000 4000
+python3 demo_train.py --gpu 0 --task 'ct' --mode 'sup' --epochs 5000 --lr 5e-4 --batch-size 2 --schedule 2000 3000 4000
 
-python3 demo_train.py --gpu 0 --task 'ct' --mode 'mc' --epochs 5000 --lr 5e-4 --batch-size 2 --ei-trans 5 --ei-alpha 100 --schedule 2000 3000 4000
+python3 demo_train.py --gpu 0 --task 'ct' --mode 'mc' --epochs 5000 --lr 5e-4  --batch-size 2 --schedule 2000 3000 4000
 
 python3 demo_train.py --gpu 0 --task 'ct' --mode 'sup_ei' --epochs 5000 --lr 5e-4 --batch-size 2 --ei-trans 5 --ei-alpha 100 --schedule 2000 3000 4000
 
